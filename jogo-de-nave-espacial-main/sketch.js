c921e45e254acd8d7a7aa5fc2efe31c12fff6515
jhonatanserio <94911517+jhonatanserio@users.noreply.github.com>
2022-07-05 10:34:55 -0300

    Add files via upload

--- jogo-de-nave-espacial-main/sketch.js
+++ jogo-de-nave-espacial-main/sketch.js
@@ -11,10 +11,13 @@ var laserjogador
 var laser2jogador
 var laserinimigo
 var enemy1
-var enemyGroup
-var inicio
-var fim
+var enemyGroup1,enemyGroup2
+var inicio=1
+var fim=0
+var laseGroupjg,laserGroupjg2,laserGroupE
 var estadoDoJogo=inicio
+var pontuacao=0
+var pontuacaoinimigo=0
 
 var enemyOne1, enemyTwo2, laser1
 
@@ -38,26 +41,32 @@ function preload() {
 function setup() {
   canvas = createCanvas(800, 600);
   starsGroup = new Group();
-  enemyGroup = createGroup();
+  
    
  
 
   jogador = createSprite(width/2,height/2+200,50,50);
   jogador.addAnimation("animaçãojogador",anigador);
   jogador.frameDelay=10
-  
-
+  enemyGroup1=new Group();;;
+  enemyGroup2=new Group();;;
+  laserGroupjg=new Group();;;
+  laserGroupjg2=new Group();;;
 }
 
 function draw(){  
 
-  background("black")
+  background("black");
+  fill ("white");
+  text("pontuação: "+pontuacao,100,50);
+  
   drawSprites();
-if(estadoDoJogo==inicio){
+ if (estadoDoJogo==inicio){
 
   if(keyDown(LEFT_ARROW)){
     jogador.x=jogador.x-4
   }
+
   if(keyDown(RIGHT_ARROW)){
     jogador.x=jogador.x+4
   }
@@ -71,18 +80,33 @@ if(estadoDoJogo==inicio){
   } else if(ran == 2){
     enemyOne(velocityX = -2)
   }}
-if(enemyGroup.isTouching(laserjogador) || enemyGroup.isTouching(laser2jogador)){
+
+  if(laserGroupjg.isTouching(enemyGroup1)){
+     console.log("fim")
+     pontuacao=pontuacao+10
+     }
+
+ if(laserGroupjg2.isTouching(enemyGroup1)){
+  console.log("fim2") 
+  pontuacao=pontuacao+10
+    }
+  if(pontuacao==10000){
     estadoDoJogo=fim
+  
   }
-if(jogador.isTouching(laserinimigo)){
-  estadoDoJogo=fim
+  if(laGroup1.isTouching()){
+    console.log("fim2") 
+    pontuacao=pontuacao+10
+  }
+  if(pontuacaoinimigo==100)
+    estadoDoJogo=fim2
+  }else if(estadoDoJogo==fim){
+    fill ("white")
+    text("parabens voce conseguiu aqui esta seu premio",300,300)
+    text("seu premio e 1200 reais por salva o mundo",300,350)
+  }
 }
-}
-else if(estadoDoJogo==fim){
-  enemyGroup.destroyEach()
-  jogador.destroy()
-}
-}
+
 
 function enemyOne(velocity){
   rotate(angle)
@@ -91,7 +115,7 @@ function enemyOne(velocity){
   enemy1.addAnimation("ENEMYONE", enemyOne1);
   enemy1.scale = 0.13;
   enemy1.velocityX = velocity;
-  
+  enemy1.lifetime = 350
 
   enemy1.x = Math.round(random(0, 800));
 
@@ -100,8 +124,10 @@ function enemyOne(velocity){
   laserinimigo.addAnimation("laser", laser1);
   laserinimigo.scale = 0.2;
   laserinimigo.velocityY= +5
-  enemyGroup.add(enemy1)
-
+  laserinimigo.lifetime=100
+  laserGroupE.add(laserinimigo)
+  enemyGroup1.add(enemy1)
+  
 }
 
 function stars(){
@@ -112,24 +138,38 @@ function stars(){
 
 
 }
-function keyPressed(){
-  if(keyCode==DOWN_ARROW){
-    laserjogador = createSprite(jogador.x, jogador.y-50);
-    laserjogador.addAnimation("laser", laser1);
-    laserjogador.scale = 0.2;
-    laserjogador.velocityY= -5
-    laserjogador.velocityX=3
-  
-    laser2jogador = createSprite(jogador.x, jogador.y-50);
-    laser2jogador.addAnimation("laser", laser1);
-    laser2jogador.scale = 0.2;
-    laser2jogador.velocityY= -5
-    laser2jogador.velocityX=-3
-}
-if(keyCode==UP_ARROW){
+function playerlaser(){
   laserjogador = createSprite(jogador.x, jogador.y-50);
   laserjogador.addAnimation("laser", laser1);
   laserjogador.scale = 0.2;
   laserjogador.velocityY= -5
+  laserjogador.velocityX=3
+  laserjogador.lifetime=100
+
+  laser2jogador = createSprite(jogador.x, jogador.y-50);
+  laser2jogador.addAnimation("laser", laser1);
+  laser2jogador.scale = 0.2;
+  laser2jogador.velocityY= -5
+  laser2jogador.velocityX=-3
+  laser2jogador.lifetime=100
+  laserGroupjg2.add(laser2jogador)
+}
+function playerlaser2(){
+  laserjogador = createSprite(jogador.x, jogador.y-50);
+  laserjogador.addAnimation("laser", laser1);
+  laserjogador.scale = 0.2;
+  laserjogador.velocityY= -5
+  laserGroupjg.add(laserjogador)
+  
+}
+function keyPressed(){
+  if(frameCount%5==0){
+  if(keyCode==DOWN_ARROW){
+  playerlaser()
+ }
+
+if(keyCode==UP_ARROW){
+  playerlaser2()
   }
+}
 }
